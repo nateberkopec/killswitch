23537bd8b627ade36163ed2698225321975ef0eb
Nate Berkopec <nate.berkopec@gmail.com>
2013-01-13 22:30:24 -0500

    POC Facebook password reset

--- lib/killswitch/switch/facebook_switch.rb
+++ lib/killswitch/switch/facebook_switch.rb
@@ -7,11 +7,21 @@ class FacebookSwitch < Switch
     @killmode = config[:killmode]
     @username = config[:username]
     @password = config[:password_hash] && BCrypt::Password.new(config[:password_hash])
+    @agent = Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
   end
 
   def kill!
     if @killmode == "password_reset"
-      "reset"
+      @agent.get('http://facebook.com/') do |page|
+        login_result = page.form_with(:action => "https://www.facebook.com/login.php?login_attempt=1") do |login|
+          login.email = @username
+          login.pass = @password
+        end.submit
+
+        settings_page = @agent.get("https://www.facebook.com/settings?ref=mb")
+        
+        #reset the pw
+      end
     elsif @killmode == "destroy"
       "destroyed"
     else
